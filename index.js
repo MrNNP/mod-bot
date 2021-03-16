a96bc8e166f0208954d6833d99722f95e053d3b7
Naishadh Patel <65918272+MrNNP@users.noreply.github.com>
2021-03-15 23:45:13 -0400

    intital commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+const bot = new Discord.Client();
+
+bot.once('ready',()=>{
+console.log('Online')
+});
+
+bot.on('message',(msg)=>{
+    if(msg.)
+
+
+});
