48f6b346e125317f982d485baeef1381b184b1ad
Naishadh Patel <65918272+MrNNP@users.noreply.github.com>
2021-03-16 23:06:28 -0400

    working state has everyone and 1/2 server ping = kick

--- index.js
+++ index.js
@@ -2,12 +2,46 @@ const Discord = require('discord.js');
 
 const bot = new Discord.Client();
 
-bot.once('ready',()=>{
-console.log('Online')
+bot.once('ready', () => {
+    console.log('Online')
 });
 
-bot.on('message',(msg)=>{
-    if(msg.)
+bot.on('message', (msg) => {
+    if (msg.mentions.everyone) {
+        try {
+            msg.channel.send('why ping');
 
+        } catch (error) {
+            errorMessage(msg, error);
+        }
+        msg.member.kick('they pinged').catch(err => {
+            errorMessage(msg, err)
+        });
+    }
+    
+    if (msg.mentions.members.array().length >= (msg.guild.memberCount / 2)) {
+        try {
+            msg.channel.send('i must ban you now');
+
+        } catch (error) {
+            errorMessage(msg, error);
+        }
+        msg.member.kick('they pinged').catch(err => {
+            errorMessage(msg, err)
+        });
+    }
 
 });
+
+var errorMessage = (msg, error) => {
+    try {
+        msg.channel.send(`i couldn't take action for some reason. here is the error if you want to try and understand ||${error}||`);
+
+    } catch (err) {
+        console.log(err);
+    }
+
+
+}
+
+bot.login('ODIxMjI2NDYxNzQ1OTA1NzA0.YFAovg.KIbjBDRv7hRrkFGkyxwSNiWYOoQ');
