11df00f8561ee8cbec20fc73e6d53b8f57bc7fd2
Naishadh Patel <65918272+MrNNP@users.noreply.github.com>
2021-03-16 23:20:50 -0400

    add js typedefs

---
+++ index.ts
@@ -0,0 +1,10 @@
+import * as Discord from 'discord.js';
+
+const bot:Discord.Client = new Discord.Client();
+
+bot.once('ready', () => {
+    console.log('Online')
+});
+
+
+bot.login('ODIxMjI2NDYxNzQ1OTA1NzA0.YFAovg.KIbjBDRv7hRrkFGkyxwSNiWYOoQ');
